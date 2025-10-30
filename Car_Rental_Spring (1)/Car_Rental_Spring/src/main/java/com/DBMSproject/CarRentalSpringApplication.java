68d7aded84e7de071ae73cdc6781883d9de697d1
Priyanshikhorwal <priyanshikhorwal0905@gmail.com>
2025-10-30 12:23:11 +0530

    this is commmit from priyanshi

--- Car_Rental_Spring (1)/Car_Rental_Spring/src/main/java/com/DBMSproject/CarRentalSpringApplication.java
+++ Car_Rental_Spring (1)/Car_Rental_Spring/src/main/java/com/DBMSproject/CarRentalSpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarRentalSpringApplication {
     // so this is the spring boot application inital step
+    //gi7y7feedfrfery6uo80mmmm
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalSpringApplication.class, args);
 	}
